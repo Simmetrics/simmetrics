905ffb36afc75557a92d55cab40e1125361a0b48
mpkorstanje <rien.korstanje@gmail.com>
2014-12-21 16:27:46 +0100

    Fixed unit test

--- src/test/java/org/simmetrics/StringMetricsTest.java
+++ src/test/java/org/simmetrics/StringMetricsTest.java
@@ -11,25 +11,20 @@ import org.simmetrics.simplifier.CaseSimplifier;
 import static org.simmetrics.StringMetrics.*;
 
 public class StringMetricsTest {
+	
 	private static final float DELTA = 0.001f;
 	private final String[] names1 = new String[] {
 			"Louis Philippe, le Roi Citoyen", "Charles X", "Louis XVIII",
 			"Napoleon II", "Napoleon I" };
-	private final String[] names2 = new String[] { "Louis XVIII",
-			"Napoléon Ier, le Grand",
-			"Louis XVI le Restaurateur de la Liberté Française" };
 
 	private final float[] expected = new float[] { 0.071f, 0.153f, 0.000f,
 			0.933f, 1.000f };
-	private final float[] expected2 = new float[] { 0.275f, 0.095f, 0.285f,
-			0.000f, 0.000f };
 
 	private StringMetric metric = new SimonWhite() {
 		{
 			setSimplifier(new CaseSimplifier.Lower());
 		}
 	};
-
 
 	@Test
 	public void testCompareList() {
@@ -48,6 +43,13 @@ public class StringMetricsTest {
 	public void testCompareArrays() {
 		assertArrayEquals(new float[0],
 				compareArrays(metric, new String[] {}, new String[] {}), DELTA);
+
+		final String[] names2 = new String[] { "Louis XVIII",
+				"Napoléon Ier, le Grand",
+				"Louis XVI le Restaurateur de la Liberté Française",
+				"Napoleon II", "Napoleon I" };
+		final float[] expected2 = new float[] { 0.275f, 0.095f, 0.285f, 1.000f,
+				1.000f };
 
 		assertArrayEquals(expected2, compareArrays(metric, names1, names2),
 				DELTA);

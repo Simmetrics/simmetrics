9aeb7daa3aa0fb03453b0d8cdd58b27b27b39b5b
mpkorstanje <rien.korstanje@gmail.com>
2015-02-21 12:44:18 +0100

    Clean up

--- simmetrics-core/src/main/java/org/simmetrics/metrics/OverlapCoefficient.java
+++ simmetrics-core/src/main/java/org/simmetrics/metrics/OverlapCoefficient.java
@@ -11,14 +11,17 @@
  * License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  * 
  * You should have received a copy of the GNU General Public License along with
  * SimMetrics. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.simmetrics.metrics;
+
+import static java.lang.Math.min;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -27,40 +30,41 @@ import org.simmetrics.SetMetric;
 
 /**
  * Implements the Overlap Coefficient algorithm providing a similarity measure
- * between two sets where it is determined to what degree a set is a subset
- * of another.
+ * between two sets where it is determined to what degree a set is a subset of
+ * another.
  * <p>
  * <code>overlap_coefficient(q,r) = (|q & r|) / min{|q|, |r|}</code>
  * 
- * @author Sam Chapman
+ * Related to the Jaccard index.
+ * 
+ * @author mpkorstanje
  * 
  * @param <T>
  *            type of the token
+ * 
+ * @see JaccardSimilarity
+ * @see <a href="http://en.wikipedia.org/wiki/Overlap_coefficient">Wikipedia -
+ *      Overlap Coefficient</a>
  */
 public final class OverlapCoefficient<T> implements SetMetric<T> {
 
 	@Override
 	public float compare(Set<T> a, Set<T> b) {
 
-		if(a.isEmpty() && b.isEmpty()){
+		if (a.isEmpty() && b.isEmpty()) {
 			return 1.0f;
 		}
-		
-		if(a.isEmpty() || b.isEmpty()){
+
+		if (a.isEmpty() || b.isEmpty()) {
 			return 0.0f;
 		}
-		
-		
-		
-		final Set<T> allTokens = new HashSet<>();
-		allTokens.addAll(a);
-		allTokens.addAll(b);
 
-		// overlap_coefficient(q,r) = ( | q & r | ) / min{ | q | , | r | }.
-		final int commonTerms = (a.size() + b.size())
-				- allTokens.size();
-		return (float) (commonTerms)
-				/ (float) Math.min(a.size(), b.size());
+		final Set<T> all = new HashSet<>();
+		all.addAll(a);
+		all.addAll(b);
+
+		final int intersection = a.size() + b.size() - all.size();
+		return intersection / (float) min(a.size(), b.size());
 	}
 
 	@Override

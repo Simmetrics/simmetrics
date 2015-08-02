a5acace4fe45b86e78e9ec6967361b3f568366c6
mpkorstanje <rien.korstanje@gmail.com>
2015-08-02 22:54:46 +0200

    Removed hand written license header

--- simmetrics-core/src/main/java/org/simmetrics/metrics/BlockDistance.java
+++ simmetrics-core/src/main/java/org/simmetrics/metrics/BlockDistance.java
@@ -1,24 +1,3 @@
-/*
- * SimMetrics - SimMetrics is a java library of Similarity or Distance Metrics,
- * e.g. Levenshtein Distance, that provide float based similarity measures
- * between String Data. All metrics return consistent measures rather than
- * unbounded similarity scores.
- * 
- * Copyright (C) 2014 SimMetrics authors
- * 
- * This file is part of SimMetrics. This program is free software: you can
- * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * SimMetrics. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.simmetrics.metrics;
 
 import static java.util.Collections.frequency;
@@ -44,7 +23,7 @@ import static java.lang.Math.abs;
  * @param <T>
  *            type of token
  */
-public class BlockDistance<T> implements ListMetric<T>, ListDistance<T>{
+public class BlockDistance<T> implements ListMetric<T>, ListDistance<T> {
 
 	@Override
 	public float compare(List<T> a, List<T> b) {
@@ -56,7 +35,6 @@ public class BlockDistance<T> implements ListMetric<T>, ListDistance<T>{
 		if (a.isEmpty() || b.isEmpty()) {
 			return 0.0f;
 		}
-
 
 		return 1.0f - distance(a, b) / (a.size() + b.size());
 	}
@@ -81,7 +59,5 @@ public class BlockDistance<T> implements ListMetric<T>, ListDistance<T>{
 	public String toString() {
 		return "BlockDistance";
 	}
-
-
 
 }

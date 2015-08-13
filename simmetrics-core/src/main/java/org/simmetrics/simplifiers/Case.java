948a46b38f1406d06e8e32b6cbde7a1fcb83c145
mpkorstanje <rien.korstanje@gmail.com>
2015-08-13 20:11:32 +0200

    Smoke tests for performance

--- simmetrics-core/src/main/java/org/simmetrics/simplifiers/Case.java
+++ simmetrics-core/src/main/java/org/simmetrics/simplifiers/Case.java
@@ -1,5 +1,10 @@
 /*
- * #%L Simmetrics Core %% Copyright (C) 2014 - 2015 Simmetrics Authors %% This
+ * #%L
+ * Simmetrics Core
+ * %%
+ * Copyright (C) 2014 - 2015 Simmetrics Authors
+ * %%
+ * This
  * program is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
@@ -11,7 +16,8 @@
  * details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>. #L%
+ * this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
 
 package org.simmetrics.simplifiers;
--- simmetrics-example/src/main/java/org/simmetrics/example/StringMetricBuilderExample.java
+++ simmetrics-example/src/main/java/org/simmetrics/example/StringMetricBuilderExample.java
@@ -1,5 +1,9 @@
 /*
- * #%L Simmetrics Examples %% Copyright (C) 2014 - 2015 Simmetrics Authors %%
+ * #%L
+ * Simmetrics Examples
+ * %%
+ * Copyright (C) 2014 - 2015 Simmetrics Authors
+ * %%
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
@@ -11,7 +15,8 @@
  * details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>. #L%
+ * this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
 
 package org.simmetrics.example;

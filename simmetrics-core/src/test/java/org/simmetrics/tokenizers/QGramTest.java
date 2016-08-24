8b83ac25ffb218725c1d3d308b0ab6892c9f84be
mpkorstanje <rien.korstanje@gmail.com>
2016-08-24 21:17:31 +0200

    Fixed QGramTest

--- simmetrics-core/src/test/java/org/simmetrics/tokenizers/QGramTest.java
+++ simmetrics-core/src/test/java/org/simmetrics/tokenizers/QGramTest.java
@@ -1,5 +1,9 @@
 /*
- * #%L Simmetrics Core %% Copyright (C) 2014 - 2016 Simmetrics Authors %%
+ * #%L
+ * Simmetrics Core
+ * %%
+ * Copyright (C) 2014 - 2016 Simmetrics Authors
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
@@ -10,7 +14,8 @@
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
- * the License. #L%
+ * the License.
+ * #L%
  */
 
 package org.simmetrics.tokenizers;
@@ -134,7 +139,7 @@ public class QGramTest {
 					// Linear-A 
 					// tokenizer should split on code points
 					new T("𐘀𐘁𐘂","𐘀𐘁", "𐘁𐘂"),
-					new T("𐘀𐘂", "𐘀𐘁", "𐘁𐘂") };
+					new T("𐘀𐘁", "𐘀𐘁") };
 		}
 	}
 
